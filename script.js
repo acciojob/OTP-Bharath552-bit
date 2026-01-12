5f74a7831181ecce508940d435cd94b0ca0f97b3
Bharath552-bit <patthembharath32@gmail.com>
2026-01-12 12:11:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const codes=document.querySelectorAll(".code")
+
+codes.forEach((code)=>{
+	code.addEventListener('keyup',(e)=>{
+		if(e.key==="Backspace"){
+			const previoussibiling=code.previousElementSibiling;
+			if(previoussibiling){
+				previoussibiling.focus()
+			}
+			return;
+		}
+		const nextsibling=code.nextElementSibiling;
+		if(nextsibling){
+			nextsibling.focus();
+		}
+	})
+})
